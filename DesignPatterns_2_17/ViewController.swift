1e9f0897f2517e8d2ea284199352427750276ae1
laurayesayan <laurayesayan@gmail.com>
2020-05-16 15:27:27 +0300

    Initial Commit

---
+++ DesignPatterns_2_17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignPatterns_2_17
+//
+//  Created by Лаура Есаян on 16.05.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
